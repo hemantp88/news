c1b91132657161ce1366831a55d48f18d14a08b3
hemantp88 <panchalhemant030700@gmail.com>
2024-06-24 12:47:09 +0530

    go goa gone

---
+++ src/App.js
@@ -0,0 +1,54 @@
+
+import './App.css';
+import LoadingBar from 'react-top-loading-bar'
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  // Switch,,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
+
+export default class App extends Component {
+  pageSize = 10;
+  // apiKey=process.env.REACT_APP_NEWS_API;
+  apiKey = "11eaac8fa362428e8d79e00287f17cf1"
+  state = {
+    progress: 0
+  }
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <LoadingBar
+            color='#f11946'
+            height={5}
+            progress={this.state.progress}
+
+          />
+
+          <Routes>
+            {/* <Route path="/about" element={<News setProgress={this.setProgress} apiKey={this.apiKey}pagesize={this.pageSize} country="us" category="sports"/></Route> */}
+            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pagesize={this.pageSize} country="us" category="general" />} />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pagesize={this.pageSize} country="us" category="business" />} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pagesize={this.pageSize} country="us" category="entertainment" />} />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pagesize={this.pageSize} country="us" category="general" />} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pagesize={this.pageSize} country="us" category="health" />} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pagesize={this.pageSize} country="us" category="science" />} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pagesize={this.pageSize} country="us" category="sports" />} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pagesize={this.pageSize} country="us" category="technology" />} />
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+}
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,38 @@
+
+import React from 'react'
+
+// export class NewsItem extends Component {
+const NewsItem = (props) => {
+  // render() {
+  let { title, description, imageurl, newsurl, author, date, source } = props;
+  let d = new Date(date).toUTCString();
+
+
+  return (
+    < div className="my-3 mx-2">
+
+      <div className="card" >
+        <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+          <span className="rounded-pill bg-danger"  >
+            {source}
+
+          </span>
+        </div>
+        <img src={!imageurl ? "https://www.hindustantimes.com/ht-img/img/2023/09/18/1600x900/Darkweb--darknet-and-hacking-concept--Hacker-with-_1690898999499_1695036729169.jpg" : imageurl} className="card-img-top" alt="..." />
+        <div className="card-body">
+
+          <h5 className="card-title">{title}
+
+          </h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text"><small className="text-body-secondary">By {!author ? "Unkown" : author} on {d} </small></p>
+
+          <a rel="noreferrer" href={newsurl} target='_blank' className="btn btn-primary btn-sm ">Read More</a>
+        </div>
+      </div>
+    </div>
+  )
+  // }
+}
+
+export default NewsItem
